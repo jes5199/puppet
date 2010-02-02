6307408c7b8c10ce903e95398d29df112f35ab1e
Jesse Wolfe <jes5199@gmail.com>
2010-02-02 11:53:05 -0800

    WIP
    Signed-off-by: Jesse Wolfe <jes5199@gmail.com>

--- lib/puppet/indirector/status/local.rb
+++ lib/puppet/indirector/status/local.rb
@@ -1,7 +1,12 @@
 require 'puppet/indirector/status'
 
 class Puppet::Indirector::Status::Local < Puppet::Indirector::Code
-    def find( *anything )
-        return model.new
+    def find( hash )
+        return model.new( hash )
+    end
+
+    def save( options )
+        content = options[:content]
+        path    = options[:path]
     end
 end
