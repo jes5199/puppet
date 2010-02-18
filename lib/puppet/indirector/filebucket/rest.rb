a02414ae717669a4fbb1f1e51f2517ab22c4393a
Jesse Wolfe <jes5199@gmail.com>
2010-02-18 14:27:54 -0800

    WIP

--- lib/puppet/indirector/filebucket/rest.rb
+++ lib/puppet/indirector/filebucket/rest.rb
@@ -2,4 +2,5 @@ require 'puppet/indirector/filebucket'
 require 'puppet/indirector/rest'
 
 class Puppet::Indirector::Filebucket::Rest < Puppet::Indirector::REST
+    desc "This is a REST based mechanism to send/retrieve file to/from the filebucket"
 end
