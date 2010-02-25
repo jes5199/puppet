de8efdbd0fdd438111cbe4e7340d7f528d5f82ae
Jesse Wolfe <jes5199@gmail.com>
2010-02-24 22:26:28 -0800

    wip

--- spec/unit/network/client/dipper.rb
+++ spec/unit/network/client/dipper.rb
@@ -2,14 +2,18 @@
 
 require File.dirname(__FILE__) + '/../../../spec_helper'
 
-describe Puppet::Network::Client.dipper do
+require 'puppet/network/client/dipper'
+describe Puppet::Network::Client::Dipper do
     it "should fail in an informative way when there are failures backing up to the server" do
         FileTest.stubs(:exists?).returns true
         File.stubs(:read).returns "content"
 
         @dipper = Puppet::Network::Client::Dipper.new(:Path => "/my/bucket")
 
-        @dipper.driver.expects(:addfile).raises ArgumentError
+        filemock = stub "bucketfile"
+        Puppet::BucketFile.stubs(:new).returns(filemock)
+        filemock.expects(:name).returns "name"
+        filemock.expects(:save).raises ArgumentError
 
         lambda { @dipper.backup("/my/file") }.should raise_error(Puppet::Error)
     end
