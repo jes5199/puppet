e24f5fbc0a20186b1177c22d49ed3b321cbb07e8
Jesse Wolfe <jes5199@gmail.com>
2010-03-04 15:15:16 -0800

    Feature #3347 REST-ified FileBucket
    FileBucket Files have been reimplemented as an indirector terminus so that
    they can be transmitted over REST.
    The old Network::Client.dipper has been replaced with a compatibility later
    in FileBucket::Dipper that uses the indirector to access filebucket termini.

---
+++ lib/puppet/file_bucket/file.rb
@@ -0,0 +1,120 @@
+require 'puppet/file_bucket'
+require 'puppet/indirector'
+
+class Puppet::FileBucket::File
+    # This class handles the abstract notion of a file in a filebucket.
+    # There are mechanisms to save and load this file locally and remotely in puppet/indirector/filebucketfile/*
+    # There is a compatibility class that emulates pre-indirector filebuckets in Puppet::FileBucket::Dipper
+    extend Puppet::Indirector
+    require 'puppet/file_bucket/file/indirection_hooks'
+    indirects :file_bucket_file, :terminus_class => :file, :extend => Puppet::FileBucket::File::IndirectionHooks
+
+    attr :path, true
+    attr :paths, true
+    attr :checksum_type
+    attr :bucket_path
+
+    def self.default_checksum_type
+        :md5
+    end
+
+    def initialize( contents, options = {} )
+        self.contents= contents
+        @bucket_path = options[:bucket_path]
+        @path        = options[:path]
+        @paths       = options[:paths] || []
+
+        @checksum_type = options[:checksum_type] || self.class.default_checksum_type
+        digest_class( @checksum_type ) # raises error on bad types
+
+        @checksum = nil # lazily calculated
+        if options[:checksum]
+            self.checksum = options[:checksum]
+        end
+    end
+
+    def checksum=(checksum)
+        validate_checksum(checksum)
+        self.checksum_type = checksum # this grabs the prefix only
+        @checksum = checksum
+    end
+
+    def validate_checksum(new_checksum, contents = @contents)
+        return unless contents
+        unless checksum_data(new_checksum) == digest_class(new_checksum).hexdigest(contents)
+            raise Puppet::Error, "checksum does not match contents"
+        end
+    end
+
+    def contents
+        @contents or raise "#{self.inspect} has no contents"
+    end
+
+    def contents=(contents)
+        raise "can't alter contents of #{self.inspect}" if @contents
+        raise ArgumentError, 'contents must be a string or nil' unless contents.nil? || contents.is_a?(String)
+
+        validate_checksum(@checksum, contents) if @checksum
+        @contents = contents
+    end
+
+    def checksum
+        @checksum ||= "#{checksum_type}:" + digest_class(checksum_type).hexdigest(contents)
+    end
+
+    def checksum_type=( new_checksum_type )
+        @checksum = nil
+        @checksum_type = checksum_type(new_checksum_type)
+    end
+
+    def checksum_type(checksum = @checksum_type)
+        checksum.to_s.split(':',2)[0].to_sym
+    end
+
+    def checksum_data(new_checksum = self.checksum)
+        new_checksum.split(':',2)[1]
+    end
+
+    def checksum_data=(new_data)
+        self.checksum = "#{checksum_type}:#{new_data}"
+    end
+
+    def digest_class(type = nil)
+        case checksum_type(type)
+        when :md5  : require 'digest/md5'  ; Digest::MD5
+        when :sha1 : require 'digest/sha1' ; Digest::SHA1
+        else
+            raise ArgumentError, "not a known checksum type: #{checksum_type(type)}"
+        end
+    end
+
+    def to_s
+        contents
+    end
+
+    def name
+        [checksum_type, checksum_data, path].compact.join('/')
+    end
+
+    def name=(name)
+        self.checksum_type, self.checksum_data, self.path = name.split('/',3)
+    end
+
+    def conflict_check?
+        true
+    end
+
+    def self.from_s( contents )
+        self.new( contents )
+    end
+
+    def to_pson
+        { "contents" => contents }.to_pson
+    end
+
+    def self.from_pson( contents )
+        structure = PSON.parse(contents)
+        self.new( structure["contents"] )
+    end
+
+end
