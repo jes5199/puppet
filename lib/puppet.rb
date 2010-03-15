8e1d12142d0cb251fa75ab713174f1b3f982b13a
Jesse Wolfe <jes5199@gmail.com>
2010-03-15 14:16:09 -0700

    Feature #3347 REST-ified FileBucket
    FileBucket Files have been reimplemented as an indirector terminus so that
    they can be transmitted over REST.
    The old Network::Client.dipper has been replaced with a compatibility later
    in FileBucket::Dipper that uses the indirector to access filebucket termini.
    
    Slightly revised patch:
    * No longer allows nil contents in FileBucket outside of initialization
    * Uses File.exist? instead of the deprecated File.exists?
    * Tweaks JSON serialization and de-serialization to include "path"
    
    Deferred issues:
    * Feature #3371 "FileBucket should not keep files in memory".
    * Feature #3372 "Replace FileBucket Dipper with more idiomatic calls"

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -172,6 +172,7 @@ require 'puppet/module'
 require 'puppet/util/storage'
 require 'puppet/parser/interpreter'
 require 'puppet/status'
+require 'puppet/file_bucket/file'
 
 if Puppet[:storeconfigs]
     require 'puppet/rails'
