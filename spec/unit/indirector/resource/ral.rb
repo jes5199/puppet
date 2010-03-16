78163067361c028b5478edaec399b65d854cd314
Jesse Wolfe <jes5199@gmail.com>
2010-03-16 15:45:13 -0700

    Eek.

---
+++ spec/unit/indirector/resource/ral.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "Puppet::Resource::Ral" do
+    describe "find" do
+        it "should find an existing instance" do
+            pending
+        end
+
+        it "if there is no instance, it should create one"
+    end
+
+    describe "search" do
+        it
+    end
+
+    describe "save" do
+        it
+    end
+end
