42fca774802890d9f69b1c749768f5ae2434c43b
Jesse Wolfe <jes5199@gmail.com>
2010-03-16 17:18:57 -0700

    ral find test 1

--- spec/unit/indirector/resource/ral.rb
+++ spec/unit/indirector/resource/ral.rb
@@ -4,8 +4,19 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 
 describe "Puppet::Resource::Ral" do
     describe "find" do
+        before do
+            @request = stub 'request', :key => "user/root"
+        end
+
         it "should find an existing instance" do
-            pending
+            my_resource    = stub "my user resource"
+
+            wrong_instance = stub "wrong user", :name => "bob"
+            my_instance    = stub "my user",    :name => "root", :to_resource => my_resource
+
+            require 'puppet/type/user'
+            Puppet::Type::User.expects(:instances).returns([ wrong_instance, my_instance, wrong_instance ])
+            Puppet::Resource::Ral.new.find(@request).should == my_resource
         end
 
         it "if there is no instance, it should create one"
