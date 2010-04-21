5e1094cbabae80a32c6382a912b71dd1e43b486d
Jesse Wolfe <jes5199@gmail.com>
2010-04-21 00:42:23 -0700

    feature #2276 Single Executable: puppet --help
    puppet --help should show generic usage, not puppet apply's help
    
    Signed-off-by: Jesse Wolfe <jes5199@gmail.com>

--- lib/puppet/util/command_line.rb
+++ lib/puppet/util/command_line.rb
@@ -4,6 +4,9 @@ module Puppet
             def self.shift_subcommand_from_argv( argv = ARGV, stdin = STDIN )
                 if ! argv.first
                     "apply" unless stdin.tty? # ttys get usage info
+                elsif argv.first == "--help"
+                    # help should give you usage, not the help for `puppet apply`
+                    nil
                 elsif argv.first =~ /^-|\.pp$|\.rb$/
                     "apply"
                 else
