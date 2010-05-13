a7a3b39407f6b70d799e6dacd4d51d5ccb7db9c7
Jesse Wolfe <jes5199@gmail.com>
2010-05-12 18:27:12 -0700

    Code cleanup: remove "self." from setdefaults
    defaults.rb was cluttered with unnessesary "self." decorations.
    I removed them in preparation for a refactor of where settings are
    handled, but that change has been postponed.
    
    Signed-off-by: Jesse Wolfe <jes5199@gmail.com>

--- lib/puppet/defaults.rb
+++ lib/puppet/defaults.rb
@@ -351,7 +351,7 @@ module Puppet
     )
 
     # Define the config default.
-    self.setdefaults(self.settings[:name],
+    setdefaults(Puppet.settings[:name],
         :config => ["$confdir/puppet.conf",
             "The configuration file for #{Puppet[:name]}."],
         :pidfile => ["$rundir/$name.pid", "The pid file"],
@@ -366,7 +366,7 @@ module Puppet
         }
     )
 
-    self.setdefaults(:master,
+    setdefaults(:master,
         :user => ["puppet", "The user puppet master should run as."],
         :group => ["puppet", "The group puppet master should run as."],
         :manifestdir => ["$confdir/manifests",
@@ -586,7 +586,7 @@ module Puppet
     )
 
     # Plugin information.
-    self.setdefaults(:main,
+    setdefaults(:main,
         :plugindest => ["$libdir",
             "Where Puppet should store plugins that it pulls down from the central
             server."],
@@ -601,7 +601,7 @@ module Puppet
     )
 
     # Central fact information.
-    self.setdefaults(:main,
+    setdefaults(:main,
         :factpath => {:default => "$vardir/lib/facter/:$vardir/facts",
             :desc => "Where Puppet should look for facts.  Multiple directories should
                 be colon-separated, like normal PATH variables.",
@@ -621,7 +621,7 @@ module Puppet
             "What files to ignore when pulling down facts."]
     )
 
-    self.setdefaults(:tagmail,
+    setdefaults(:tagmail,
         :tagmap => ["$confdir/tagmail.conf",
             "The mapping between reporting tags and email addresses."],
         :sendmail => [%x{which sendmail 2>/dev/null}.chomp,
@@ -632,7 +632,7 @@ module Puppet
             "The server through which to send email reports."]
     )
 
-    self.setdefaults(:rails,
+    setdefaults(:rails,
         :dblocation => { :default => "$statedir/clientconfigs.sqlite3",
             :mode => 0660,
             :owner => "service",
