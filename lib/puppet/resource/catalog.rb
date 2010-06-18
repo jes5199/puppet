e3b8a12f93f3f4883006685156fd7d69c87fddca
Jesse Wolfe <jes5199@gmail.com>
2010-06-18 11:59:11 -0700

    wip: fix filter

--- lib/puppet/resource/catalog.rb
+++ lib/puppet/resource/catalog.rb
@@ -382,7 +382,7 @@ class Puppet::Resource::Catalog < Puppet::SimpleGraph
     end
 
     def resource_refs
-        resource_keys.collect{ |type, name| name.is_a? String ? "#{type}[#{name}]" : nil}.compact
+        resource_keys.collect{ |type, name| name.is_a?( String ) ? "#{type}[#{name}]" : nil}.compact
     end
 
     def resource_keys
